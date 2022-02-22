fe1968cfa00c0552873ac7580fb2902236d2bbfe
Agustin Sartorio <sartorio85@gmail.com>
2022-02-22 19:25:44 -0300

    index inicial

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,12 @@
-import type { NextPage } from 'next'
-import Head from 'next/head';
-import Hero from '../components/Hero';
-import NavBar from '../components/NavBar';
-import Walkthrough from '../components/Walkthrough';
+import type { NextPage } from "next";
+import Head from "next/head";
+import Image from "next/image";
+import Link from "next/link";
+import Catalog from "../components/Catalog";
+import Hero from "../components/Hero";
+import NavBar from "../components/NavBar";
+import Walkthrough from "../components/Walkthrough";
+import GitHub from "../public/assets/icons/github-default.svg";
 
 const Home: NextPage = () => {
   return (
@@ -13,8 +17,17 @@ const Home: NextPage = () => {
       <NavBar />
       <Hero />
       <Walkthrough />
+      <Catalog />
+      <footer className="footer_index">
+        <Link href="https://github.com/Agus-Sartorio/aeroLabChallenge" passHref>
+          <a>
+            <Image src={GitHub} alt="github-logo" />
+            <span>View this repository</span>
+          </a>
+        </Link>
+      </footer>
     </>
-  )
-}
+  );
+};
 
 export default Home;
