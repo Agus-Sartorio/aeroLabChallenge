b3016a740f15ed21a1ccc00b1e6a7e863f631112
Agustin Sartorio <sartorio85@gmail.com>
2022-02-22 19:25:23 -0300

    context creado

--- context/index.tsx
+++ context/index.tsx
@@ -3,28 +3,25 @@ import { createContext, useState, Dispatch, SetStateAction } from "react";
 export const Context = createContext<Value | null>(null);
 
 interface Props {
-    children: JSX.Element
+  children: JSX.Element;
 }
 
 interface User {
-    puntos: number,
-    productos: []
+  puntos: number;
+  productos: [];
 }
 
 interface Value {
-    user: User,
-    setUser: Dispatch<SetStateAction<User>>
+  user: User;
+  setUser: Dispatch<SetStateAction<User>>;
 }
 
 const UserProvider = ({ children }: Props) => {
+  const [user, setUser] = useState<User>({ puntos: 0, productos: [] });
 
-    const [user, setUser] = useState<User>({ puntos: 0, productos: [] })
-
-    return (
-        <Context.Provider value={{ user, setUser }}>
-            {children}
-        </Context.Provider>
-    )
-}
+  return (
+    <Context.Provider value={{ user, setUser }}>{children}</Context.Provider>
+  );
+};
 
 export default UserProvider;
