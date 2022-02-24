736be1f53559d16b0aa5b836b1ab8334ac31d00d
Agustin Sartorio <sartorio85@gmail.com>
2022-02-24 20:49:26 -0300

    CEO

--- pages/index.tsx
+++ pages/index.tsx
@@ -12,6 +12,45 @@ const Home: NextPage = () => {
   return (
     <>
       <Head>
+        <meta name="theme-color" content="#ff80ff" />
+        <meta
+          name="description"
+          content="Desafio de front-end hecho por Agustin Sartorio para AerloLab"
+        />
+        <meta
+          property="og:title"
+          content="Desafio AeroLab - Agustin Sartorio"
+        />
+        <meta property="og:locale" content="es_AR" />
+        <meta
+          property="og:url"
+          content="https://aero-lab-challenge.vercel.app"
+        />
+        <meta property="og:type" content="website" />
+        <meta
+          property="og:description"
+          content="Desafio de front-end hecho por Agustin Sartorio para AerloLab"
+        />
+        <meta property="og:image" content="https://i.imgur.com/afIg9Zl.png" />
+
+        <meta name="twitter:card" content="summary" />
+        <meta
+          name="twitter:title"
+          content="Desafio AerloLab - Agustin Sartorio"
+        />
+        <meta
+          name="twitter:url"
+          content="https://aero-lab-challenge.vercel.app"
+        />
+        <meta
+          name="twitter:description"
+          content="Desafio de front-end hecho por Agustin Sartorio para AerloLab"
+        />
+        <meta name="twitter:image" content="https://i.imgur.com/afIg9Zl.png" />
+        <meta
+          name="twitter:image:alt"
+          content="Desafio AerloLab - Agustin Sartorio"
+        />
         <title>Aerolab Challenge</title>
       </Head>
       <NavBar />
