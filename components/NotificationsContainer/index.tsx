f5ef17901349774cdbedf5c3d0a13c0005ced483
Agustin Sartorio <sartorio85@gmail.com>
2022-02-27 18:54:39 -0300

    console.log eliminado

--- components/NotificationsContainer/index.tsx
+++ components/NotificationsContainer/index.tsx
@@ -11,25 +11,30 @@ export default function NotificationsContainer() {
   const { user } = useContext(Context)!;
 
   useEffect(() => {
-    if(!user.productos.length) return
-    const lastProduct = user.productos[user.productos.length - 1]
-    setActiveNotifications((prev) => [...prev, lastProduct._id])
-  }, [user.productos.length]) // eslint-disable-line react-hooks/exhaustive-deps
+    if (!user.productos.length) return;
+    const lastProduct = user.productos[user.productos.length - 1];
+    setActiveNotifications((prev) => [...prev, lastProduct._id]);
+  }, [user.productos.length]); // eslint-disable-line react-hooks/exhaustive-deps
 
   const close = (id: string) => {
-    setActiveNotifications((prev) => prev.filter(n => n !== id))
-  }
-  console.log(user.productos)
+    setActiveNotifications((prev) => prev.filter((n) => n !== id));
+  };
 
   return (
     <Container>
-      {
-        user.productos.map(p => {
-          return( activeNotifications.includes(p._id) &&
-            <Notification close={close} productName={p.name} id={p._id} key={notificationId++} type='success'/>
+      {user.productos.map((p) => {
+        return (
+          activeNotifications.includes(p._id) && (
+            <Notification
+              close={close}
+              productName={p.name}
+              id={p._id}
+              key={notificationId++}
+              type="success"
+            />
           )
-        })
-      }
+        );
+      })}
     </Container>
   );
 }
